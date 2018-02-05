a8f1cc5f0cdce08591f9587103a43f86745e6625
mohammed Alnakhli <2229683a@student.gla.ac.uk>
2018-02-05 21:17:44 +0000

    chapter 7

--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib import admin
 from rango.models import Category, Page
-
+from django import forms
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
